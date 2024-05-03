12d0c0ae9cdc72e9627131db0a21682a5862f861
Hoskar Jubeleo <hoskajubeleo@gmail.com>
2024-05-03 01:48:52 -0300

    style: update category item

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({category}: CategoryItemProps) => {
     return ( 
-        <div className="flex min-w-fit items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">  
+        <div className="flex justify-center min-w-fit items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">  
             <Image src={category.imageUrl} alt={category.name} width={30} height={30}/>
 
             <span className="font-semibold text-sm">{category.name}</span>
