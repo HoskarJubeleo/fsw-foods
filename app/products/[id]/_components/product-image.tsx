478d73e955bb61bedc8dfc645c907d6785303771
Hoskar Jubeleo <hoskajubeleo@gmail.com>
2024-05-07 00:43:01 -0300

    improvement: bug resolution product image

--- app/products/[id]/_components/product-image.tsx
+++ app/products/[id]/_components/product-image.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
+import { Product } from "@prisma/client";
 import { ChevronLeftIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
