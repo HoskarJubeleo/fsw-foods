9ad958d224d55b6b0d6054ca3d287b43c5ef7755
Hoskar Jubeleo <hoskajubeleo@gmail.com>
2024-05-08 01:57:41 -0300

    feat: add link in header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const header = () => {
   return (
-    <div className="flex justify-between">
-      <Image src="/logo.png" alt="FSW Foods" width={130} height={30} />
+    <div className="flex justify-between px-5 pt-6">
+      <div className="relative h-[30px] w-[100px]">
+        <Link href="/">
+        <Image src="/logo.png" alt="FSW Foods" fill className="object-cover" />
+        </Link>
+      </div>
       <Button
         size="icon"
         variant="outline"
