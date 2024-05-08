d73aa8c232c09f8629ee8f56d090a725537d502d
Hoskar Jubeleo <hoskajubeleo@gmail.com>
2024-05-08 02:28:24 -0300

    fix: correct formatting in code

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -8,7 +8,12 @@ const header = () => {
     <div className="flex justify-between px-5 pt-6">
       <div className="relative h-[30px] w-[100px]">
         <Link href="/">
-        <Image src="/logo.png" alt="FSW Foods" fill className="object-cover" />
+          <Image
+            src="/logo.png"
+            alt="FSW Foods"
+            fill
+            className="object-cover"
+          />
         </Link>
       </div>
       <Button
